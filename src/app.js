c84f8887e428c0fb10ce2f18a34e3d4c12829640
JuliLuT <tacconijulieta18@gmail.com>
2022-10-17 19:19:39 -0300

    Rutaspublic

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const path = require('path')
+const app = express()
+const PORT = 3000
+app.listen(PORT, () => console.log("listening on port", PORT))
+app.use(express.static('public'))
